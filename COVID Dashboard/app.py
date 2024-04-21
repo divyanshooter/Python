1da79944cfdc3afb75337d90963731e0875890d1
Divyanshu Chaturvedi <divyanshooter@gmail.com>
2024-04-22 00:29:15 +0530

    Covid Dashboard graphs added and readme addded

--- COVID Dashboard/app.py
+++ COVID Dashboard/app.py
@@ -14,18 +14,35 @@ external_stylesheets= [{
 
 }]
 
+patients=pd.read_csv('IndividualDetails.csv')
+total=patients.shape[0]
+active=patients[patients['current_status']=='Hospitalized'].shape[0]
+recovered=patients[patients['current_status']=='Recovered'].shape[0]
+deaths=patients[patients['current_status']=='Deceased'].shape[0]
+
+covid=pd.read_csv('covid_19_india.csv',parse_dates=["Date"])
+covid_date=covid.groupby('Date')['Confirmed'].sum().reset_index()
+
+agewise=pd.read_csv("AgeGroupDetails.csv")
+
+options=[
+    {"label":"All","value":"All"},
+    {"label":"Hospitalized","value":"Hospitalized"},
+    {"label":"Recovered","value":"Recovered"},
+    {"label":"Deceased","value":"Deceased"},
+]
+
+
 app=dash.Dash(__name__,external_stylesheets=external_stylesheets)
 
-
-if __name__=="__main__":
-    app.layout=html.Div([
-        html.H1("CoVID India View",style={"color":"#fff","text-align":"center","margin-top":"40px"}),
+app.layout=html.Div([
+        html.H1("COVID-19 India View",style={"color":"#fff","text-align":"center","margin-top":"40px"}),
         html.Div([
             html.Div([
                 html.Div([
                     html.Div([
                         html.H3("Total Cases",className="text-light"),
-                        html.H4("600",className="text-light")
+                        html.H4(total,className="text-light")
                     ],className="card-body")
                 ],className="card bg-warning")
             ],className="col-md-3"),
@@ -33,29 +50,61 @@ if __name__=="__main__":
                 html.Div([
                     html.Div([
                         html.H3("Active Cases", className="text-light"),
-                        html.H4("600", className="text-light")
+                        html.H4(active, className="text-light")
                     ],className="card-body")
                 ],className="card bg-info")
             ],className="col-md-3"),
             html.Div([html.Div([
                     html.Div([
                         html.H3("Recovered", className="text-light"),
-                        html.H4("600", className="text-light")
+                        html.H4(recovered, className="text-light")
                     ],className="card-body")
                 ],className="card bg-success")],className="col-md-3"),
             html.Div([html.Div([
                     html.Div([
                         html.H3("Deaths", className="text-light"),
-                        html.H4("600", className="text-light")
+                        html.H4(deaths, className="text-light")
                     ],className="card-body")
                 ],className="card bg-danger")],className="col-md-3")
+        ],className="row mt-5"),
+        html.Div([
+            html.Div([
+                dcc.Graph(id="Line",figure=go.Figure(data= go.Line(x=covid_date["Date"],y=covid_date["Confirmed"]),
+                                            layout=go.Layout(title="Day by Day Analysis")))
+            ], className="col-md-6 mt-5"),
+            html.Div([
+                dcc.Graph(id="Pie", figure=go.Figure(data=go.Pie(labels=agewise["AgeGroup"], values=agewise["TotalCases"]),
+                                                      layout=go.Layout(title="Age Wise Analysis")))
+            ], className="col-md-6 mt-5"),
         ],className="row"),
         html.Div([
-            html.Div([], className="col-md-6"),
-            html.Div([], className="col-md-6"),
-        ],className="row"),
-        html.Div([
-            html.Div([], className="col-md-12"),
+            html.Div([
+                html.Div([
+                    html.Div([
+                        dcc.Dropdown(id="picker",options=options,value="All"),
+                        dcc.Graph(id="bar")
+                    ],className="card-body")
+                ],className="card")]
+                , className="col-md-12 mt-5"),
 ],className="row")
     ],className="container")
+
+
+@app.callback(Output("bar","figure"),[Input("picker","value")])
+def update_graph(type):
+    if type=="All":
+        pbar = patients["detected_state"].value_counts().reset_index()
+        return {
+            "data": [go.Bar(x=pbar["detected_state"],y=pbar["count"])],
+            "layout": go.Layout(title="State Wise Count")
+        }
+    else :
+        npat=patients[patients["current_status"]==type]
+        pbar = npat["detected_state"].value_counts().reset_index()
+        return {
+            "data": [go.Bar(x=pbar["detected_state"], y=pbar["count"])],
+            "layout": go.Layout(title="State Wise Count")
+        }
+
+if __name__=="__main__":
     app.run_server(debug=True)
---
+++ COVID Dashboard/readme.md
@@ -0,0 +1,64 @@
+COVID-19 India Dashboard
+
+This dashboard provides comprehensive visualizations and statistics regarding COVID-19 cases in India. It is a web-based application built using Python's Dash framework and Plotly for interactive data visualization.
+
+Features
+
+1. Overview Section:
+Total Cases: Displays the total number of COVID-19 cases reported in India.
+Active Cases: Shows the current active cases requiring hospitalization.
+Recovered: Indicates the total number of patients who have recovered from COVID-19.
+Deaths: Displays the total number of deaths due to COVID-19.
+
+2. Day-by-Day Analysis:
+Line Chart: Visualizes the trend of confirmed cases over time, providing insights into the spread of the virus.
+
+3. Age-wise Analysis:
+Pie Chart: Presents the distribution of COVID-19 cases across different age groups, allowing users to understand the demographics affected by the virus.
+
+4. State-wise Analysis:
+Bar Chart: Illustrates the distribution of COVID-19 cases by state, enabling users to identify regions with the highest number of cases.
+Dynamic Filtering: Users can filter the state-wise analysis based on the status of cases, including hospitalized, recovered, and deceased.
+
+Data Sources
+The dashboard utilizes the following data sources:
+
+Individual Patient Data: sourced from the IndividualDetails.csv file, providing detailed information about each COVID-19 patient.
+Daily COVID-19 Statistics: obtained from the covid_19_india.csv file, offering daily updates on confirmed cases.
+Age Group Data: sourced from the AgeGroupDetails.csv file, providing insights into the distribution of cases across different age groups.
+Dependencies
+The following dependencies are required to run the dashboard:
+
+Python 3.x
+numpy
+pandas
+plotly
+dash
+dash-html-components
+dash-core-components
+Installation
+To set up the dashboard locally, follow these steps:
+
+1.Clone this repository:
+git clone https://github.com/your_username/COVID19-India-Dashboard.git
+
+2.Navigate to the project directory:
+cd COVID19-India-Dashboard
+
+3.Install dependencies:
+pip install -r requirements.txt
+
+Usage
+
+1.Run the Dash application:
+python app.py
+
+2.Open a web browser and go to http://127.0.0.1:8050/ to access the dashboard.
+
+3.Interact with the dashboard to explore COVID-19 statistics and visualizations.
+
+This project is licensed under the MIT License. See the LICENSE file for details.
+
+Acknowledgements
+Special thanks to the contributors of the COVID-19 data sources used in this dashboard.
+We appreciate the efforts of the Dash and Plotly development teams for providing powerful tools for data visualization.
